6c22324b66b9575bcc4622942fb979935db5609e
Farid Ibrahimzada <ibrahimzadef26@gmail.com>
2022-12-18 00:26:01 +0400

    adding todoapp to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
+  const [name,setName] = useState("");  
+  const [userName,setUserName] = useState("");  
+  const [age,setAge] = useState("");
+  const [list, setList] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name,userName,age);
+    const date = {name,userName, age};
+    if (name && userName && age) {
+      setList((ls)=> [...ls,date]);
+      setName("");
+      setUserName("");
+      setAge("");
+    }
+  };
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Todo</h1>
+      <form onSubmit={handleSubmit}>
+        <input placeholder='Name' name='name' value={name} onChange = {(e)=>setName(e.target.value)}/> <br/>
+        <input placeholder='Username' name='name' value={userName} onChange = {(e)=> setUserName(e.target.value)}/> <br/>
+        <input placeholder='Age' type='number' value={age} onChange = {(e)=> setAge(e.target.value)}/>
+        <button>Add</button>
+      </form>
+      {
+        list.map((t)=> <div>
+          <li>{t.name}</li>
+          <li>{t.userName}</li>
+          <li>{t.age}</li>
+        </div>)
+      }
     </div>
-  );
+  )
 }
 
 export default App;
